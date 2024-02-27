d08ddb0e4e6f9edda9eda786516562d013b3a57d
Tabish Nadeem <tabishnadeem50@gmail.com>
2024-02-28 01:52:34 +0530

    Update vite.config.ts

--- pokerplanning/vite.config.ts
+++ pokerplanning/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 5000,
+    strictPort: true,
+  },
+  server: {
+    port: 5000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5000",
+  }
 })
